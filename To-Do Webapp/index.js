67ab4bcd12849691de280cff002b95fcf5d1c868
Chimaroke Amadi <chirokeamadi@gmail.com>
2024-03-25 22:57:41 +0100

    added variables and "addTask" function to js file

---
+++ To-Do Webapp/index.js
@@ -0,0 +1,10 @@
+const taskId = document.getElementById('task_id');
+const taskDescription = document.getElementById('task_description');
+
+function addTask() {
+    if (taskId.value === '' || taskDescription.value === ''){
+        alert("Please fill the required inputs first.");
+    } else{
+
+    }
+}
