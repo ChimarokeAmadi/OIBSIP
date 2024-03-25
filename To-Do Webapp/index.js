f228bf6b37047685b2d4652db3012dde92d94bb3
Chimaroke Amadi <chirokeamadi@gmail.com>
2024-03-25 23:17:05 +0100

    added "listContainer" variable to js file

--- To-Do Webapp/index.js
+++ To-Do Webapp/index.js
@@ -1,10 +1,13 @@
 const taskId = document.getElementById('task_id');
 const taskDescription = document.getElementById('task_description');
+const listContainer = document.getElementById('list_container');
 
 function addTask() {
     if (taskId.value === '' || taskDescription.value === ''){
         alert("Please fill the required inputs first.");
     } else{
+        let li = document.createElement("li");
+        li.innerHTML = "<div>" + "<p>" + taskId.value + "</p>" + "<p>" + taskDescription.value + "</p>"
 
     }
 }
